7d1d9697f109c6075485e4449a9980db5112c49b
currycan <ansandy@foxmail.com>
2019-02-14 15:35:13 +0800

    Update tcp.sh

--- install/tcp.sh
+++ install/tcp.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-set -ex
+set -e
 
 github="raw.githubusercontent.com/chiakge/Linux-NetSpeed/master"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
