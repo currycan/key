352cf61ac26c67ac9a5dd51d3cd59f0b847c94a4
currycan <ansandy@foxmail.com>
2019-03-25 16:06:55 +0800

    优化脚本

--- install/initial.sh
+++ install/initial.sh
@@ -39,9 +39,9 @@ check_version(){
 }
 
 optimizing_system(){
-    wget --no-check-certificate -O /etc/sysctl.conf https://raw.githubusercontent.com/currycan/key/master/sysctl.conf
-    wget --no-check-certificate -O /etc/security/limits.conf https://raw.githubusercontent.com/currycan/key/master/limits.conf
-    FLAG_PROFILE=$(grep "ulimit -SHn 1000000" /etc/profile | wc -l)
+    sudo \curl -SLo /etc/sysctl.conf https://raw.githubusercontent.com/currycan/key/master/sysctl.conf
+    sudo \curl -SLo /etc/security/limits.conf https://raw.githubusercontent.com/currycan/key/master/limits.conf
+    FLAG_PROFILE=$(sudo grep "ulimit -SHn 1000000" /etc/profile | wc -l)
     if [ FLAG_PROFILE == 0 ];then
         echo "ulimit -SHn 1000000">>/etc/profile
     fi
@@ -49,14 +49,14 @@ optimizing_system(){
 
 pip_install(){
     if [ $(which pip | wc -l) == 0 ];then
-        wget --no-check-certificate -O ./get-pip.py https://bootstrap.pypa.io/get-pip.py
-        python get-pip.py
+        sudo curl -SLo ./get-pip.py https://bootstrap.pypa.io/get-pip.py
+        sudo python get-pip.py
         rm -f get-pip.py
     else
         VERSION=$(pip --version |tr -s ' '| cut -d' ' -f2 | cut -d'.' -f1)
         if [ $VERSION != 19 ];then
-            wget --no-check-certificate -O ./get-pip.py https://bootstrap.pypa.io/get-pip.py
-            python get-pip.py
+            sudo curl -SLo ./get-pip.py https://bootstrap.pypa.io/get-pip.py
+            sudo python get-pip.py
             rm -f get-pip.py
         fi
     fi
@@ -65,31 +65,39 @@ pip_install(){
 user_init(){
     mkdir -p /app
     FLAG_GROUP=$(grep andrew /etc/group | wc -l)
-    if [ $FLAG_GROUP == 0 ];then 
+    if [ $FLAG_GROUP == 0 ];then
         groupadd andrew
     fi
-    if [ ! -f /app/andrew/.bashrc ];then 
+    if [ ! -f /app/andrew/.bashrc ];then
         useradd -m andrew -g andrew -s /bin/bash -d /app/andrew
     fi
 }
 
 download_ssh_key(){
-    mkdir -p /app/andrew/.ssh/
-    wget --no-check-certificate -O /app/andrew/.ssh/authorized_keys https://raw.githubusercontent.com/currycan/key/master/authorized_keys
-    wget --no-check-certificate -O /app/andrew/.ssh/id_rsa.pub https://raw.githubusercontent.com/currycan/key/master/id_rsa.pub
-    chmod 600  /app/andrew/.ssh/*
-    chown -R andrew:andrew /app/andrew/.ssh/
+    sudo mkdir -p ssh /app/andrew/.ssh/ /root/.ssh/
+    sudo curl -SLo ssh/id_rsa.pub https://raw.githubusercontent.com/currycan/key/master/id_rsa.pub
+    sudo curl -SLo ssh/authorized_keys https://raw.githubusercontent.com/currycan/key/master/authorized_keys
+    sudo cp -a ssh/* /app/andrew/.ssh/
+    sudo cp -a ssh/* /root/.ssh/
+    sudo chmod 700 /app/andrew/.ssh/
+    sudo chmod 600 /app/andrew/.ssh/*
+    sudo chown -R andrew:andrew /app/andrew/.ssh/
+    sudo chmod -R 600 /root/.ssh/
+    sudo chown -R root:root /root/.ssh/
+    sudo rm -rf ssh
 }
 
 yum_init(){
-    yum update -y
-    yum install -y sudo vim wget net-tools telnet lrzsz lsof bash-completion epel-release python psmisc crond git
+    sudo yum update -y
+    sudo yum install -y sudo vim wget net-tools telnet lrzsz lsof bash-completion epel-release python psmisc crond git
     pip_install
+    pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+    pip config set global.trusted-host mirrors.aliyun.com
     pip install -U speedtest-cli
 }
 
 apt_init(){
-    apt update 
+    apt update
     apt upgrade -y
     apt install -y sudo vim wget net-tools telnet lrzsz lsof bash-completion python curl psmisc cron git
     pip_install
@@ -106,13 +114,18 @@ from pip import __main__
 if __name__ == '__main__':
     sys.exit(__main__._main())
 EOF
+    pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+    pip config set global.trusted-host mirrors.aliyun.com
     pip install -U speedtest-cli
 }
 
 ssh_init(){
-    wget --no-check-certificate -O /etc/ssh/sshd_config https://raw.githubusercontent.com/currycan/key/master/sshd_config
-    wget --no-check-certificate -O ~/.bashrc https://raw.githubusercontent.com/currycan/key/master/bashrc
-    wget --no-check-certificate -O /app/andrew/.bashrc https://raw.githubusercontent.com/currycan/key/master/bashrc
+    sudo sed -i /etc/ssh/sshd_config -e "s/^PasswordAuthentication.*/PasswordAuthentication yes/g"
+    sudo sed -i /etc/ssh/sshd_config -e "s/^#PubkeyAuthentication.*/PubkeyAuthentication yes/g"
+    sudo sed -i /etc/ssh/sshd_config -e "s/^[#]*PermitRootLogin.*/PermitRootLogin yes/g"
+
+    sudo \curl -SLo/root/.bashrc https://raw.githubusercontent.com/currycan/key/master/bashrc
+    sudo \curl -SLo /app/andrew/.bashrc https://raw.githubusercontent.com/currycan/key/master/bashrc
     chown -R andrew:andrew /app/andrew/.bashrc
 }
 
@@ -124,53 +137,45 @@ initial(){
     user_init
     download_ssh_key
     if [[ "${release}" == "centos" ]]; then
-        echo "ZZT520.596msl*18" | passwd --stdin root
-        echo "zzt2008zzt" | passwd --stdin andrew
+        sudo echo "ZZT520.596msl*18" | passwd --stdin root
+        sudo echo "zzt2008zzt" | passwd --stdin andrew
         chcon -R unconfined_u:object_r:user_home_t:s0 /app/
         yum_init
         FLAG_SUDO=$(grep andrew /etc/sudoers | wc -l)
         if [ $FLAG_SUDO == 0 ];then
-            sed -i '93i  andrew    ALL=(ALL:ALL) NOPASSWD: ALL' /etc/sudoers
+            # sudo sed -i '93i  andrew    ALL=(ALL:ALL) NOPASSWD: ALL' /etc/sudoers
+            sudo sed -i '101i  %andrew    ALL=(ALL:ALL) NOPASSWD: ALL' /etc/sudoers
         fi
         if [[ ${version} == "6" ]]; then
             echo "centos 6"
         elif [[ ${version} == "7" ]]; then
-            systemctl stop firewalld && systemctl disable firewalld
+            sudo systemctl stop firewalld && systemctl disable firewalld
         else
             echo "unkown error"
         fi
         ssh_init
-        echo "Subsystem sftp /usr/lib/openssh/sftp-server" >> /etc/ssh/sshd_config
-        echo 'export PS1="[\[\e[31;43m\]\[\e[5m\]\u\[\e[0m\]\[\e[37;40m\]@\[\e[32;40m\]\h\[\e[33;40m\] \w\[\e[0m\]]\\$ "' >> ~/.bashrc
-        echo 'export PS1="[\u@\h \W]\$"' >> /app/andrew/.bashrc
-        sed "s/nofile 6553600/nofile 65536/g" -i /etc/security/limits.conf
-        service sshd restart
+        sudo echo 'export PS1="[\[\e[31;43m\]\[\e[5m\]\u\[\e[0m\]\[\e[37;40m\]@\[\e[32;40m\]\h\[\e[33;40m\] \w\[\e[0m\]]\\$ "' >> ~/.bashrc
+        sudo echo 'export PS1="[\u@\h \W]\$"' >> /app/andrew/.bashrc
+        sudo sed "s/nofile 6553600/nofile 65536/g" -i /etc/security/limits.conf
         echo "Done~"
     elif [[ "${release}" == "debian" ]] || [[ "${release}" == "ubuntu" ]]; then
-        echo "root:ZZT520.596msl*18" | chpasswd
-        echo "andrew:zzt2008zzt" | chpasswd
+        sudo echo "root:ZZT520.596msl*18" | chpasswd
+        sudo echo "andrew:zzt2008zzt" | chpasswd
         apt_init
-        systemctl stop firewalld && systemctl disable firewalld
+        sudo systemctl stop firewalld && systemctl disable firewalld
         FLAG_SUDO=$(grep andrew /etc/sudoers | wc -l)
         if [ $FLAG_SUDO == 0 ];then
-            sed -i '21i  andrew    ALL=(ALL:ALL) NOPASSWD: ALL' /etc/sudoers
+            sudo sed -i '21i  %andrew    ALL=(ALL:ALL) NOPASSWD: ALL' /etc/sudoers
         fi
         ssh_init
-        echo "Subsystem sftp /usr/lib/openssh/sftp-server" >> /etc/ssh/sshd_config
-        echo 'export PS1="\[\e[31;43m\]\[\e[5m\]\u\[\e[0m\]\[\e[37;40m\]@\[\e[32;40m\]\h\[\e[33;40m\]:\w\[\e[0m\]\\$ "' >> ~/.bashrc
-        echo 'export PS1="\u@\h:\w\$ "' >> /app/andrew/.bashrc
-        service sshd restart
+        sudo echo 'export PS1="\[\e[31;43m\]\[\e[5m\]\u\[\e[0m\]\[\e[37;40m\]@\[\e[32;40m\]\h\[\e[33;40m\]:\w\[\e[0m\]\\$ "' >> ~/.bashrc
+        sudo echo 'export PS1="\u@\h:\w\$ "' >> /app/andrew/.bashrc
         echo "Done~"
     else
         echo "unkown system"
     fi
-    stty erase '^H' && read -p "需要重启VPS，是否现在重启 ? [Y/n] :" yn
-    [ -z "${yn}" ] && yn="y"
-    if [[ $yn == [Yy] ]]; then
-        echo -e "${Info} VPS 重启中..."
-        reboot
-    fi
-    sysctl -p
+    service sshd restart
+    sudo sysctl -p
 }
 
 initial | tee initial.log
