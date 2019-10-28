33d796238f17d78a7063e8f321c4c99bbb6f988b
zhangzhitao <zhangzhitao@lakala.com>
2019-10-28 11:00:34 +0800

    创建Python3 软链

--- install/initial.sh
+++ install/initial.sh
@@ -89,13 +89,9 @@ download_ssh_key(){
 
 yum_init(){
     sudo yum update -y
-    sudo yum install -y sudo vim wget net-tools telnet lrzsz lsof bash-completion epel-release psmisc git
-    if [[ ${version} != "8" ]]; then
-        yum install -y python3
-    elif [[ ${version} == "8" ]]; then
-        yum install -y python36
-    else
-        echo "unkown error"
+    sudo yum install -y sudo vim wget net-tools telnet lrzsz lsof bash-completion epel-release python3 psmisc git
+    if [[ ${version} == "8" ]]; then
+        ln -sf /usr/bin/python3 /usr/bin/python
     fi
     pip_install
     # pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
