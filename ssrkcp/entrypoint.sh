0704658384aa68d27bb578ea5f18ca09d35cd77f
zhangzhitao <zhangzhitao@lakala.com>
2019-12-28 23:24:06 +0800

    no message

--- ssrkcp/entrypoint.sh
+++ ssrkcp/entrypoint.sh
@@ -4,6 +4,7 @@ set -eou pipefail
 
 # allow the container to be started with `--user`
 if [ "${1#-}" = 'ss-server' -a "$(id -u)" = '0' ]; then
+  [[ ${enable_kcptun} == true ]] && set -- "$@" "--plugin kcptun-server-plugin --plugin-opts \"key=${key};raw=${raw}\""
   set -- "/sbin/tini" "$@"
   # exec gosu ssrkcp "$@"
   # exec su-exec ssrkcp "$@"
