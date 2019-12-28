dd526937c214dd5bb55644d848b5b0a46e7b85ea
zhangzhitao <zhangzhitao@lakala.com>
2019-12-28 19:00:54 +0800

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,14 @@
 
 set -exou pipefail
 
-if [ "${1:0:1}" = '-' ]; then
-    set -- /usr/bin/ss-server -s ${server_addr} \
-    -p ${server_port} \
-    -k ${password:-$(hostname)} \
-    -m ${method} \
-    -t ${timeout} \
-    -d ${dns_addrs} \
-    -u \
-    --fast-open \
-    --mtu ${mtu} \
-    --no-delay \
-    ${args} \
-    [[ ${enable_kcptun} == true ]] && --plugin kcptun-server-plugin --plugin-opts
+if [ "${1#-}" != "$1" ]; then
+  set -- ss-server "$@"
 fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'ss-server' -a "$(id -u)" = '0' ]; then
+  find . \! -user ssrkcp -exec chown ssrkcp '{}' +
+  exec gosu ssrkcp "$0" "$@"
+fi
+
 exec "$@"
