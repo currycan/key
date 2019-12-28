23971e6feef45291390b039c81b5ebcf99067525
zhangzhitao <zhangzhitao@lakala.com>
2019-12-28 16:58:39 +0800

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
-#!/bin/sh
+#!/bin/sh -e
 
-/usr/bin/ss-server -s ${server_addr} \
-  -p ${server_port} \
-  -k ${password:-$(hostname)} \
-  -m ${method} \
-  -t ${timeout} \
-  -d ${dns_addrs} \
-  -u \
-  --fast-open \
-  --mtu 1200 \
-  --no-delay \
-  ${args} \
-  $1
+set -exou pipefail
 
+if [ "${1:0:1}" = '-' ]; then
+    set -- /usr/bin/ss-server -s ${server_addr} \
+    -p ${server_port} \
+    -k ${password:-$(hostname)} \
+    -m ${method} \
+    -t ${timeout} \
+    -d ${dns_addrs} \
+    -u \
+    --fast-open \
+    --mtu ${mtu} \
+    --no-delay \
+    ${args} \
+    "$@"
+fi
+exec "$@"
