626b29f775ed9f3930f497e7bb0e9a72902ab776
zhangzhitao <zhangzhitao@lakala.com>
2020-01-01 19:10:45 +0800

    no message

--- install/initial.sh
+++ install/initial.sh
@@ -92,7 +92,7 @@ yum_init(){
     sudo yum install -y sudo vim wget net-tools telnet lrzsz lsof bash-completion epel-release python3 psmisc git
     yum install -y yum-utils device-mapper-persistent-data lvm2
     yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    yum install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+    rpm -Uvh --nodeps --force https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
     yum install -y docker-ce docker-ce-cli containerd.io
     if [[ ${version} == "8" ]]; then
         ln -sf /usr/bin/python3 /usr/bin/python
