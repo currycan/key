e68a5dfc510fbfc1da8cad79c1d2dbabef511bfa
zhangzhitao <zhangzhitao@lakala.com>
2020-01-15 11:00:53 +0800

    no message

--- ssrkcp/sources/utils/view_config.sh
+++ ssrkcp/sources/utils/view_config.sh
@@ -23,23 +23,26 @@ get_ip() {
 	[[ -z $ip ]] && echo -e "\n$red 这垃圾小鸡扔了吧！$none\n" && exit
 }
 
-# ss + kcptun link
-ss_kcptun_link(){
+# ss + kcptun show
+ss_kcptun_show(){
     local link_head="ss://"
     local cipher_pwd=$(get_str_base64_encode "${SS_METHOD}:${SS_PASSWD}")
     local ip_port_plugin="@$(get_ip):${KCP_PORT}/?plugin=${PLUGIN_CLIENT_NAME}"
-    local plugin_opts=";sndwnd=${KCP_RCVWND};rcvwnd=${KCP_SNDWND};datashard=${KCP_DATASHARD};parityshard=${KCP_PARITYSHARD};mtu=${KCP_MTU};mode=${KCP_MODE};crypt=${KCP_CRYPT};key=${KCP_PASSWD};dscp=${KCP_DSCP};conn=2;autoexpire=1800;scavengettl=600;sockbuf=16777217;smuxbuf=16777217"
+    local plugin_opts="sndwnd=${KCP_RCVWND};rcvwnd=${KCP_SNDWND};datashard=${KCP_DATASHARD};parityshard=${KCP_PARITYSHARD};mtu=${KCP_MTU};mode=${KCP_MODE};crypt=${KCP_CRYPT};key=${KCP_PASSWD};dscp=${KCP_DSCP};smuxver=${KCP_SMUXVER};conn=2;keepalive=10;autoexpire=1800;scavengettl=600;sockbuf=16777217;smuxbuf=16777217;streambuf=2097152"
     if [[ ${KCP_NOCOMP} == true ]];then
       plugin_opts=${plugin_opts}";nocomp=${KCP_NOCOMP}"
-      if [[ ${KCP_TCP} == true ]]; then
-        plugin_opts=${plugin_opts}";tcp=${KCP_TCP}"
-      fi
+      # if [[ ${KCP_TCP} == true ]]; then
+      #   plugin_opts=${plugin_opts}";tcp=${KCP_TCP}"
+      # fi
     fi
-    ss_link="${link_head}${cipher_pwd}${ip_port_plugin}${plugin_opts}"
-}
-
-# ss + kcptun show
-ss_kcptun_show(){
+    ss_link="${link_head}${cipher_pwd}${ip_port_plugin};${plugin_opts}"
+    local kcptun_paras="--sndwnd ${KCP_RCVWND} --rcvwnd ${KCP_SNDWND} --datashard ${KCP_DATASHARD} --parityshard ${KCP_PARITYSHARD} --mtu ${KCP_MTU} --mode ${KCP_MODE} --crypt ${KCP_CRYPT} --key ${KCP_PASSWD} --dscp ${KCP_DSCP} --smuxver ${KCP_SMUXVER} --conn 2 --keepalive 10 --autoexpire 1800 --scavengettl 600 --sockbuf 16777217 --smuxbuf 16777217 --streambuf 2097152"
+    if [[ ${KCP_NOCOMP} == true ]];then
+      kcptun_paras=${kcptun_paras}" --nocomp ${KCP_NOCOMP}"
+      # if [[ ${KCP_TCP} == true ]]; then
+      #   kcptun_paras=${kcptun_paras}" --tcp ${KCP_TCP}"
+      # fi
+    fi
     echo >> ${HUMAN_CONFIG}
     echo -e " Shadowsocks的配置信息：" >> ${HUMAN_CONFIG}
     echo >> ${HUMAN_CONFIG}
@@ -49,19 +52,9 @@ ss_kcptun_show(){
     echo -e " 加密     : ${Red}${SS_METHOD}${suffix}" >> ${HUMAN_CONFIG}
     echo -e " 插件程序 : ${Red}${PLUGIN_CLIENT_NAME}${suffix}" >> ${HUMAN_CONFIG}
     echo -e " 插件选项 :                                      " >> ${HUMAN_CONFIG}
-    if [[ ${KCP_NOCOMP} == false ]] && [[ ${KCP_TCP} == false ]]; then
-        echo -e " 插件参数 : ${Red}-l %SS_LOCAL_HOST%:%SS_LOCAL_PORT% -r %SS_REMOTE_HOST%:%SS_REMOTE_PORT% --sndwnd ${KCP_RCVWND} --rcvwnd ${KCP_SNDWND} --datashard ${KCP_DATASHARD} --parityshard ${KCP_PARITYSHARD} --mtu ${KCP_MTU} --mode ${KCP_MODE}  --crypt ${KCP_CRYPT} --key ${KCP_PASSWD} --dscp ${KCP_DSCP}${suffix}" >> ${HUMAN_CONFIG}
-        echo >> ${HUMAN_CONFIG}
-        echo -e " 手机参数 : sndwnd=${KCP_RCVWND};rcvwnd=${KCP_SNDWND};datashard=${KCP_DATASHARD};parityshard=${KCP_PARITYSHARD};mtu=${KCP_MTU};mode=${KCP_MODE};crypt=${KCP_CRYPT};key=${KCP_PASSWD};dscp=${KCP_DSCP}" >> ${HUMAN_CONFIG}
-    elif [[ ${KCP_NOCOMP} == true ]] && [[ ${KCP_TCP} == false ]]; then
-        echo -e " 插件参数 : ${Red}-l %SS_LOCAL_HOST%:%SS_LOCAL_PORT% -r %SS_REMOTE_HOST%:%SS_REMOTE_PORT% --sndwnd ${KCP_RCVWND} --rcvwnd ${KCP_SNDWND} --datashard ${KCP_DATASHARD} --parityshard ${KCP_PARITYSHARD} --mtu ${KCP_MTU} --mode ${KCP_MODE}  --crypt ${KCP_CRYPT} --key ${KCP_PASSWD} --dscp ${KCP_DSCP} --nocomp ${KCP_NOCOMP}${suffix}" >> ${HUMAN_CONFIG}
-        echo >> ${HUMAN_CONFIG}
-        echo -e " 手机参数 : sndwnd=${KCP_RCVWND};rcvwnd=${KCP_SNDWND};datashard=${KCP_DATASHARD};parityshard=${KCP_PARITYSHARD};mtu=${KCP_MTU};mode=${KCP_MODE};crypt=${KCP_CRYPT};key=${KCP_PASSWD};dscp=${KCP_DSCP};nocomp=${KCP_NOCOMP}" >> ${HUMAN_CONFIG}
-    else
-        echo -e " 插件参数 : ${Red}-l %SS_LOCAL_HOST%:%SS_LOCAL_PORT% -r %SS_REMOTE_HOST%:%SS_REMOTE_PORT% --sndwnd ${KCP_RCVWND} --rcvwnd ${KCP_SNDWND} --datashard ${KCP_DATASHARD} --parityshard ${KCP_PARITYSHARD} --mtu ${KCP_MTU} --mode ${KCP_MODE}  --crypt ${KCP_CRYPT} --key ${KCP_PASSWD} --dscp ${KCP_DSCP} --nocomp ${KCP_NOCOMP} --tcp ${KCP_TCP}${suffix}" >> ${HUMAN_CONFIG}
-        echo >> ${HUMAN_CONFIG}
-        echo -e " 手机参数 : sndwnd=${KCP_RCVWND};rcvwnd=${KCP_SNDWND};datashard=${KCP_DATASHARD};parityshard=${KCP_PARITYSHARD};mtu=${KCP_MTU};mode=${KCP_MODE};crypt=${KCP_CRYPT};key=${KCP_PASSWD};dscp=${KCP_DSCP};nocomp=${KCP_NOCOMP};tcp=${KCP_TCP}" >> ${HUMAN_CONFIG}
-    fi
+    echo -e " 插件参数 : ${Red}-l %SS_LOCAL_HOST%:%SS_LOCAL_PORT% -r %SS_REMOTE_HOST%:%SS_REMOTE_PORT% ${kcptun_paras}${suffix}" >> ${HUMAN_CONFIG}
+    echo >> ${HUMAN_CONFIG}
+    echo -e " 手机参数 : ${plugin_opts}" >> ${HUMAN_CONFIG}
     echo >> ${HUMAN_CONFIG}
     echo -e " SS  链接 : ${Green}${ss_link}${suffix}" >> ${HUMAN_CONFIG}
     echo >> ${HUMAN_CONFIG}
@@ -72,7 +65,6 @@ ss_kcptun_show(){
 }
 
 show_config(){
-    ss_kcptun_link
     ss_kcptun_show
     local mark=$1
     if [ -e $HUMAN_CONFIG ]; then
