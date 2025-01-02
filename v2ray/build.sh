91dc40854d68b3c8a55eecfb49ff68ec9ecd51ca
currycan <ansandy@foxmail.com>
2025-01-02 17:38:52 +0800

    Apply .gitattributes to enforce LF

--- v2ray/build.sh
+++
@@ -1,3 +0,0 @@
-# /bin/env bash
-
-docker build -t currycan/v2ray-web:3.0.0 .
--- v2ray/script/ssl_update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-rc-service nginx stop
-sleep 1
-${HOME}/.acme.sh/acme.sh --cron --home ${HOME}/.acme.sh &>/dev/null
-${HOME}/.acme.sh/acme.sh --installcert -d ${DOMAIN} --fullchainpath ${SSL_PATH}/${DOMAIN}.crt --keypath ${SSL_PATH}/${DOMAIN}.key --ecc --force
-sleep 1
-rc-service nginx start
