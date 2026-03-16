4633c59439531575b76a85e42981d6c848afb7db
currycan <ansandyzzt@gmail.com>
2026-03-16 15:47:38 +0800

    feat: Hysteria2 启用 Salamander 混淆，对抗晚高峰 UDP QoS
    - sing-box inbound: 添加 obfs salamander（password=SB_UUID）
    - proxies/all、clash、stash: 添加 obfs/obfs-password 字段
    - proxies/surge: 添加 obfs=salamander,obfs-password= 参数
    - show-config.sh: URI 添加 obfs=salamander&obfs-password= 参数
    
    Salamander 将 QUIC 包伪装为随机 UDP 噪音，DPI 无法识别为代理流量，
    有效规避国内 ISP 晚高峰针对 QUIC 特征的 QoS 限速与干扰。

--- docker/sb-xray/scripts/show-config.sh
+++ docker/sb-xray/scripts/show-config.sh
@@ -102,7 +102,7 @@ generate_links() {
     local vmes_json="{\"v\":\"2\",\"ps\":\"${FLAG_PREFIX}Vmess ✈ ${region_name}${NODE_SUFFIX}\",\"add\":\"${CDNDOMAIN}\",\"port\":\"${LISTENING_PORT}\",\"id\":\"${XRAY_UUID}\",\"aid\":\"0\",\"scy\":\"auto\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"${CDNDOMAIN}\",\"path\":\"/${XRAY_URL_PATH}-vmessws\",\"tls\":\"tls\",\"sni\":\"${CDNDOMAIN}\",\"alpn\":\"h2\",\"fp\":\"chrome\"}"
 
     # 基础链接 (Clash 支持部分)
-    local link_hysteria2="hysteria2://${SB_UUID}@${DOMAIN}:${PORT_HYSTERIA2}/?sni=${DOMAIN}&${h2_alpn}#${FLAG_PREFIX}Hysteria2 ✈ ${region_name}${NODE_SUFFIX}"
+    local link_hysteria2="hysteria2://${SB_UUID}@${DOMAIN}:${PORT_HYSTERIA2}/?sni=${DOMAIN}&obfs=salamander&obfs-password=${SB_UUID}&${h2_alpn}#${FLAG_PREFIX}Hysteria2 ✈ ${region_name}${NODE_SUFFIX}"
     local link_tuic="tuic://${SB_UUID}:${SB_UUID}@${DOMAIN}:${PORT_TUIC}?${h2_alpn}&congestion_control=bbr#${FLAG_PREFIX}TUIC ✈ ${region_name}${NODE_SUFFIX}"
     local link_anytls="anytls://${SB_UUID}@${DOMAIN}:${PORT_ANYTLS}?security=tls&type=tcp#${FLAG_PREFIX}AnyTLS ✈ ${region_name}${NODE_SUFFIX}"
     local link_vmess="vmess://$(echo -n "$vmes_json" | base64 -w0)"
